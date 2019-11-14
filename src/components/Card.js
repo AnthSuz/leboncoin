cc202be98ad5676d772e4980aac5950b3461edcb
Anthony Suzanne <anthonysuzanne@mbp-de-anthony.lan>
2019-11-14 15:15:39 +0100

    card : done

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,20 @@ const Card = () => {
     <div className="card">
       <div className="wrapper">
         <div className="card--inside">
-          <img src={Photo} alt="photo annonce" />
-          <p>Peugeot 308 Allure SetS 130 CV EAT 6</p>
-          <p>17 500€</p>
-          <p>13/11/2019 à 18:10</p>
+          <div className="card--img">
+            <img src={Photo} alt="photo annonce" />
+          </div>
+          <div className="card--txt">
+            <div className="card--txt--top">
+              <span className="card--title">
+                Peugeot 308 Allure SetS 130 CV EAT 6
+              </span>
+              <span className="card--price">17 500€</span>
+            </div>
+            <div classname="card--txt--bottom">
+              <span className="card--date">13/11/2019 à 18:10</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/containers/Offers.js
+++ src/containers/Offers.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
-// import Card from "../components/Card";
+import Card from "../components/Card";
 
 const Offers = () => {
   return (
     <>
       <Header />
       <Search />
-      {/* <Card /> */}
+      <Card />
     </>
   );
 };
